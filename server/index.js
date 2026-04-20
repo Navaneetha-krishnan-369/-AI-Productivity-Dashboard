f59858f6a4a726f431071365e93fe1fb4fa14a8b
Navaneetha-krishnan-369 <navaneethonepiece@gmail.com>
2026-04-20 14:46:29 +0530

    Dynamic port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import { GoogleGenAI } from '@google/genai';
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
